7ffd68f5a8d16bcd7a5ee307c48a6bca2ab05935
Jack <jratermann@unr.edu>
2025-09-17 23:59:29 -0700

    Release Brew Tap

---
+++ Sherlock.rb
@@ -0,0 +1,12 @@
+cask "Sherlock" do
+  version "1.8.0"
+  sha256 "798e8b12e9f1479d6948570cd3215eafcde78ec90a3a370575efd7d8bcb75ffa"
+
+  url "https://github.com/Ap0ll02/Sherlock/releases/download/1.8/Sherlock.zip"
+  name "Sherlock"
+  desc "A different spotlight search for MacOS"
+  homepage "https://github.com/Ap0ll02/Sherlock"
+
+  app "Sherlock.app"
+end
+
