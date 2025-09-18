77a1a6b286898aa8871e6f851b019d2970772f81
Jack <jratermann@unr.edu>
2025-09-18 00:03:11 -0700

    Better Naming

--- Sherlock.rb
+++ Sherlock.rb
@@ -1,9 +1,9 @@
-cask "Sherlock" do
+cask "SherlockBar" do
   version "1.8.0"
   sha256 "798e8b12e9f1479d6948570cd3215eafcde78ec90a3a370575efd7d8bcb75ffa"
 
   url "https://github.com/Ap0ll02/Sherlock/releases/download/1.8/Sherlock.zip"
-  name "Sherlock"
+  name "SherlockBar"
   desc "A different spotlight search for MacOS"
   homepage "https://github.com/Ap0ll02/Sherlock"
 
