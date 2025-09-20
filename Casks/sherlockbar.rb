bac4f573241998df1e9f010b403d6aa11f409d92
Jack <jratermann@unr.edu>
2025-09-20 07:24:55 -0700

    Release 1.85

--- Casks/sherlockbar.rb
+++ Casks/sherlockbar.rb
@@ -1,8 +1,8 @@
 cask "sherlockbar" do
-  version "1.8.2"
-  sha256 "abb44cee44ab73d2fbb486d1760dd5aaff3f5adc1490755909bd99cf3189621e"
+  version "1.8.5"
+  sha256 "292fceb2c15590440ef5d8e63fa01df1369737dbf1eb5533597b58cad276de68"
 
-  url "https://github.com/Ap0ll02/Sherlock/releases/download/1.82/Sherlock.zip"
+  url "https://github.com/Ap0ll02/Sherlock/releases/download/1.85/Sherlock.zip"
   name "SherlockBar"
   desc "A different spotlight search for MacOS"
   homepage "https://github.com/Ap0ll02/Sherlock"
